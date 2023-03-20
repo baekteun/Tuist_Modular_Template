ec4963ac67f73cfad39b16cee25433ad09a5ae43
baegteun <baegteun@gmail.com>
2023-03-20 14:23:35 +0900

    :memo: :: 모듈 경로 .targetName -> rawValue

--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
@@ -11,31 +11,31 @@ public extension TargetDependency {
 public extension TargetDependency.Feature {
     static let BaseFeature = TargetDependency.project(
         target: ModulePaths.Feature.BaseFeature.targetName(type: .sources),
-        path: .relativeToFeature(ModulePaths.Feature.BaseFeature.targetName(type: .sources))
+        path: .relativeToFeature(ModulePaths.Feature.BaseFeature.rawValue)
     )
 }
 
 public extension TargetDependency.Domain {
     static let BaseDomain = TargetDependency.project(
         target: ModulePaths.Domain.BaseDomain.targetName(type: .sources),
-        path: .relativeToDomain(ModulePaths.Domain.BaseDomain.targetName(type: .sources))
+        path: .relativeToDomain(ModulePaths.Domain.BaseDomain.rawValue)
     )
 }
 
 public extension TargetDependency.Core {
     static let DesignSystem = TargetDependency.project(
         target: ModulePaths.Core.DesignSystem.targetName(type: .sources),
-        path: .relativeToCore(ModulePaths.Core.DesignSystem.targetName(type: .sources))
+        path: .relativeToCore(ModulePaths.Core.DesignSystem.rawValue)
     )
 }
 
 public extension TargetDependency.Shared {
     static let UtilityModule = TargetDependency.project(
         target: ModulePaths.Shared.UtilityModule.targetName(type: .sources),
-        path: .relativeToShared(ModulePaths.Shared.UtilityModule.targetName(type: .sources))
+        path: .relativeToShared(ModulePaths.Shared.UtilityModule.rawValue)
     )
     static let GlobalThirdPartyLibrary = TargetDependency.project(
         target: ModulePaths.Shared.GlobalThirdPartyLibrary.targetName(type: .sources),
-        path: .relativeToShared(ModulePaths.Shared.GlobalThirdPartyLibrary.targetName(type: .sources))
+        path: .relativeToShared(ModulePaths.Shared.GlobalThirdPartyLibrary.rawValue)
     )
 }
